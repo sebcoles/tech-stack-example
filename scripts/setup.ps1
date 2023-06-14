5c225c72fcfd00992e08fa76b661f6e78862685d
Seb Coles <seb.coles@outlook.com>
2023-06-14 22:07:04 +0100

    pipelines

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,4 +1,4 @@
-Install-Module -Name PSMermaidTools
-Install-Module -Name powrshell-yaml
+Install-Module -Name PSMermaidTools -Force
+Install-Module -Name powershell-yaml -Force
 Import-Module PSMermaidTools
 Import-module powershell-yaml
--- scripts/teams.ps1
+++ scripts/teams.ps1
@@ -8,17 +8,17 @@ foreach($company in $companies) {
 
 
 # Read YAML files
-$teams = Get-Content -Raw "../$($company)/teams.yml"
+$teams = Get-Content -Raw "$($company)/teams.yml"
 $teamsObject = ConvertFrom-YAML $teams
 
-$domains = Get-Content -Raw "../$($company)/domains.yml"
+$domains = Get-Content -Raw "$($company)/domains.yml"
 $domainsObject = ConvertFrom-YAML $domains
 
-$techStack = Get-Content -Raw "../$($company)/tech-stack.yml"
+$techStack = Get-Content -Raw "$($company)/tech-stack.yml"
 $techStackObject = ConvertFrom-YAML $techStack
 
 # Create output folder
-$path = "../output/$($company)" 
+$path = "output/$($company)" 
 if((Test-Path -Path $path) -eq $false){
   New-Item -Path $path -ItemType Directory  | Out-Null
 }
@@ -44,5 +44,5 @@ if((Test-Path -Path $path) -eq $false){
   # Generate HTML
   $html = Get-Content .\template.html
   $replaced = $html.replace("#{replace}#", $mermaidString)
-  $replaced | Out-File -FilePath "../output/$($company)/teams_$(get-date -f yyyy-MM-dd).html"
+  $replaced | Out-File -FilePath "output/$($company)/teams_$(get-date -f yyyy-MM-dd).html"
 }
